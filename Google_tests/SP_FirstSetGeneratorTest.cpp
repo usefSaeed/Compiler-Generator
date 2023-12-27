2903a5d6a162a71d235c87fa822ec7340bd2724a
usefSaeed <youssef.okab@gmail.com>
2023-12-28 00:17:49 +0200

    first-test 4 success

--- Google_tests/SP_FirstSetGeneratorTest.cpp
+++ Google_tests/SP_FirstSetGeneratorTest.cpp
@@ -90,3 +90,126 @@ TEST(FirstSetGeneration,NonTerminalScenario){
     ASSERT_EQ(nts[0]->getFirstSet().get()->getSet(), fs_expected.getSet());
 }
 
+
+TEST(FirstSetGeneration,LateFirstCase){
+    /// A --> B C D | C B a
+    /// B --> C a | \\L
+    /// C --> c | \\L
+    /// D --> d | b
+    std::shared_ptr<NonTerminal> A = std::make_shared<NonTerminal>("A");
+    std::shared_ptr<NonTerminal> B = std::make_shared<NonTerminal>("B");
+    std::shared_ptr<NonTerminal> C = std::make_shared<NonTerminal>("C");
+    std::shared_ptr<NonTerminal> D = std::make_shared<NonTerminal>("D");
+    std::shared_ptr<Terminal> a = std::make_shared<Terminal>("a");
+    std::shared_ptr<Terminal> b = std::make_shared<Terminal>("b");
+    std::shared_ptr<Terminal> c = std::make_shared<Terminal>("c");
+    std::shared_ptr<Terminal> d = std::make_shared<Terminal>("d");
+    std::shared_ptr<Terminal> eps = std::make_shared<Terminal>("\\L");
+    Grammar::epsilon = eps.get();
+
+    std::vector<std::vector<std::shared_ptr<Symbol>>> vv_s;
+    std::vector<std::shared_ptr<Symbol>> v_s;
+    v_s.push_back(B); v_s.push_back(C); v_s.push_back(D); vv_s.push_back(v_s);
+    v_s.clear();
+    v_s.push_back(C); v_s.push_back(B); v_s.push_back(a); vv_s.push_back(v_s);
+    A->setProductions(vv_s);
+
+    v_s.clear(); vv_s.clear();
+    v_s.push_back(C); v_s.push_back(a); vv_s.push_back(v_s);
+    v_s.clear();
+    v_s.push_back(eps); vv_s.push_back(v_s);
+    B->setProductions(vv_s);
+
+    v_s.clear(); vv_s.clear();
+    v_s.push_back(c); vv_s.push_back(v_s);
+    v_s.clear();
+    v_s.push_back(eps); vv_s.push_back(v_s);
+    C->setProductions(vv_s);
+
+    v_s.clear(); vv_s.clear();
+    v_s.push_back(d); vv_s.push_back(v_s);
+    v_s.clear();
+    v_s.push_back(b); vv_s.push_back(v_s);
+    D->setProductions(vv_s);
+
+    v_s.clear(); vv_s.clear();
+
+    std::vector<std::shared_ptr<NonTerminal>> nts = {A,B,C,D};
+
+    FirstSetsGenerator firstSG(nts);
+
+    FirstSet fs_expected;
+    fs_expected.add(c.get()); fs_expected.add(eps.get());
+    ASSERT_EQ(nts[2]->getFirstSet().get()->getSet(), fs_expected.getSet());
+    fs_expected.add(a.get());
+    ASSERT_EQ(nts[1]->getFirstSet().get()->getSet(), fs_expected.getSet());
+    fs_expected.removeEpsilon();
+    fs_expected.add(b.get());fs_expected.add(d.get());
+    ASSERT_EQ(nts[0]->getFirstSet().get()->getSet(), fs_expected.getSet());
+    fs_expected.clear();
+    fs_expected.add(b.get());fs_expected.add(d.get());
+    ASSERT_EQ(nts[3]->getFirstSet().get()->getSet(), fs_expected.getSet());
+}
+
+TEST(FirstSetGeneration,LateEpsilonCase){
+    /// A --> B C D | C B a
+    /// B --> C a | \\L
+    /// C --> c | \\L
+    /// D --> d | b | \\L
+
+    std::shared_ptr<NonTerminal> A = std::make_shared<NonTerminal>("A");
+    std::shared_ptr<NonTerminal> B = std::make_shared<NonTerminal>("B");
+    std::shared_ptr<NonTerminal> C = std::make_shared<NonTerminal>("C");
+    std::shared_ptr<NonTerminal> D = std::make_shared<NonTerminal>("D");
+    std::shared_ptr<Terminal> a = std::make_shared<Terminal>("a");
+    std::shared_ptr<Terminal> b = std::make_shared<Terminal>("b");
+    std::shared_ptr<Terminal> c = std::make_shared<Terminal>("c");
+    std::shared_ptr<Terminal> d = std::make_shared<Terminal>("d");
+    std::shared_ptr<Terminal> eps = std::make_shared<Terminal>("\\L");
+    Grammar::epsilon = eps.get();
+
+    std::vector<std::vector<std::shared_ptr<Symbol>>> vv_s;
+    std::vector<std::shared_ptr<Symbol>> v_s;
+    v_s.push_back(B); v_s.push_back(C); v_s.push_back(D); vv_s.push_back(v_s);
+    v_s.clear();
+    v_s.push_back(C); v_s.push_back(B); v_s.push_back(a); vv_s.push_back(v_s);
+    A->setProductions(vv_s);
+
+    v_s.clear(); vv_s.clear();
+    v_s.push_back(C); v_s.push_back(a); vv_s.push_back(v_s);
+    v_s.clear();
+    v_s.push_back(eps); vv_s.push_back(v_s);
+    B->setProductions(vv_s);
+
+    v_s.clear(); vv_s.clear();
+    v_s.push_back(c); vv_s.push_back(v_s);
+    v_s.clear();
+    v_s.push_back(eps); vv_s.push_back(v_s);
+    C->setProductions(vv_s);
+
+    v_s.clear(); vv_s.clear();
+    v_s.push_back(d); vv_s.push_back(v_s);
+    v_s.clear();
+    v_s.push_back(b); vv_s.push_back(v_s);
+    v_s.clear();
+    v_s.push_back(eps); vv_s.push_back(v_s);
+    D->setProductions(vv_s);
+
+    v_s.clear(); vv_s.clear();
+
+    std::vector<std::shared_ptr<NonTerminal>> nts = {A,B,C,D};
+
+    FirstSetsGenerator firstSG(nts);
+
+    FirstSet fs_expected;
+    fs_expected.add(c.get()); fs_expected.add(eps.get());
+    ASSERT_EQ(nts[2]->getFirstSet().get()->getSet(), fs_expected.getSet());
+    fs_expected.add(a.get());
+    ASSERT_EQ(nts[1]->getFirstSet().get()->getSet(), fs_expected.getSet());
+    fs_expected.add(b.get());fs_expected.add(d.get());
+    ASSERT_EQ(nts[0]->getFirstSet().get()->getSet(), fs_expected.getSet());
+    fs_expected.clear();
+    fs_expected.add(b.get());fs_expected.add(d.get());fs_expected.add(eps.get());
+    ASSERT_EQ(nts[3]->getFirstSet().get()->getSet(), fs_expected.getSet());
+}
+
