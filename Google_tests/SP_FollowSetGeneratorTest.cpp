7558784a46f3ec255ba31ac0be98ceb8d8a76387
usefSaeed <youssef.okab@gmail.com>
2023-12-28 04:19:44 +0200

    test5 follow done

--- Google_tests/SP_FollowSetGeneratorTest.cpp
+++ Google_tests/SP_FollowSetGeneratorTest.cpp
@@ -263,3 +263,112 @@ TEST(FollowSetGeneration,CircularScenario){
     ASSERT_EQ(D->getFollowSet().get()->getSet(), fs_expected.getSet());
     ASSERT_EQ(C->getFollowSet().get()->getSet(), fs_expected.getSet());
 }
+
+TEST(FollowSetGeneration,Complex){
+    /// E --> T E`
+    /// E` --> + E | \\L
+    /// T --> F T`
+    /// T` --> T | \\L
+    /// F --> P F`
+    /// F` --> * F | \\L
+    /// P --> ( E ) | a | b | Em
+    /// FIRST(E) = FIRST(F) = FIRST(T) = FIRST(P) = { a, b, (, Em }
+    /// FIRST(T`) = { a, b, (, Em, \\L }
+    /// FIRST(E') = { +, \\L }
+    /// FIRST(F') = { *, \\L }
+    /// FOLLOW(E) = FOLLOW(E`) = { $, +, ) }
+    /// FOLLOW(T) = FOLLOW(T`) = { $, +, ) }
+    /// FOLLOW(E) = FOLLOW(E`) = { $, ), (, a, b, Em, + }
+    /// FOLLOW(P) = { $, ), (, a, b, Em, +, * }
+
+    std::shared_ptr<NonTerminal> E = std::make_shared<NonTerminal>("E");
+    std::shared_ptr<NonTerminal> E_d = std::make_shared<NonTerminal>("E`");
+    std::shared_ptr<NonTerminal> T = std::make_shared<NonTerminal>("T");
+    std::shared_ptr<NonTerminal> T_d = std::make_shared<NonTerminal>("T`");
+    std::shared_ptr<NonTerminal> F = std::make_shared<NonTerminal>("F");
+    std::shared_ptr<NonTerminal> F_d = std::make_shared<NonTerminal>("F`");
+    std::shared_ptr<NonTerminal> P = std::make_shared<NonTerminal>("P");
+    std::shared_ptr<Terminal> plus = std::make_shared<Terminal>("+");
+    std::shared_ptr<Terminal> ast = std::make_shared<Terminal>("*");
+    std::shared_ptr<Terminal> lb = std::make_shared<Terminal>("(");
+    std::shared_ptr<Terminal> rb = std::make_shared<Terminal>(")");
+    std::shared_ptr<Terminal> a = std::make_shared<Terminal>("a");
+    std::shared_ptr<Terminal> b = std::make_shared<Terminal>("b");
+    std::shared_ptr<Terminal> Em = std::make_shared<Terminal>("Em");
+    std::shared_ptr<Terminal> eps = std::make_shared<Terminal>("\\L");
+    Grammar::epsilon = eps.get();
+
+    std::vector<std::vector<std::shared_ptr<Symbol>>> vv_s;
+    std::vector<std::shared_ptr<Symbol>> v_s;
+    v_s.push_back(T); v_s.push_back(E_d); vv_s.push_back(v_s);
+    E->setProductions(vv_s);
+
+    v_s.clear(); vv_s.clear();
+    v_s.push_back(plus); v_s.push_back(E); vv_s.push_back(v_s);
+    v_s.clear();
+    v_s.push_back(eps); vv_s.push_back(v_s);
+    E_d->setProductions(vv_s);
+
+    v_s.clear(); vv_s.clear();
+    v_s.push_back(F);v_s.push_back(T_d); vv_s.push_back(v_s);
+    T->setProductions(vv_s);
+
+    v_s.clear(); vv_s.clear();
+    v_s.push_back(T); vv_s.push_back(v_s);
+    v_s.clear();
+    v_s.push_back(eps); vv_s.push_back(v_s);
+    T_d->setProductions(vv_s);
+
+    v_s.clear(); vv_s.clear();
+    v_s.push_back(P); v_s.push_back(F_d); vv_s.push_back(v_s);
+    F->setProductions(vv_s);
+
+    v_s.clear(); vv_s.clear();
+    v_s.push_back(ast); v_s.push_back(F); vv_s.push_back(v_s);
+    v_s.clear();
+    v_s.push_back(eps); vv_s.push_back(v_s);
+    F_d->setProductions(vv_s);
+
+    v_s.clear(); vv_s.clear();
+    v_s.push_back(lb); v_s.push_back(E); v_s.push_back(rb); vv_s.push_back(v_s);
+    v_s.clear();
+    v_s.push_back(a); vv_s.push_back(v_s);
+    v_s.clear();
+    v_s.push_back(b); vv_s.push_back(v_s);
+    v_s.clear();
+    v_s.push_back(Em); vv_s.push_back(v_s);
+    P->setProductions(vv_s);
+
+
+    std::shared_ptr<FirstSet> tempFS = std::make_shared<FirstSet>();
+    tempFS->add(a.get()); tempFS->add(b.get()); tempFS->add(lb.get()); tempFS->add(Em.get());
+    E->getFirstSet()->addAll(tempFS);
+    T->getFirstSet()->addAll(tempFS);
+    F->getFirstSet()->addAll(tempFS);
+    P->getFirstSet()->addAll(tempFS);
+    tempFS->add(eps.get());
+    T_d->getFirstSet()->addAll(tempFS);
+    tempFS->clear();
+    tempFS->add(eps.get());tempFS->add(plus.get());
+    E_d->getFirstSet()->addAll(tempFS);
+    tempFS->clear();
+    tempFS->add(eps.get());tempFS->add(ast.get());
+    F_d->getFirstSet()->addAll(tempFS);
+
+    std::vector<std::shared_ptr<NonTerminal>> nts = {E,E_d,T,T_d,F,F_d,P};
+
+    FollowSetsGenerator followSG(nts,E.get());
+
+    FollowSet fs_expected;
+    fs_expected.add(rb.get());fs_expected.addEOI();
+    ASSERT_EQ(E->getFollowSet().get()->getSet(), fs_expected.getSet());
+    ASSERT_EQ(E_d->getFollowSet().get()->getSet(), fs_expected.getSet());
+    fs_expected.add(plus.get());
+    ASSERT_EQ(T->getFollowSet().get()->getSet(), fs_expected.getSet());
+    ASSERT_EQ(T_d->getFollowSet().get()->getSet(), fs_expected.getSet());
+    fs_expected.add(a.get()); fs_expected.add(b.get()); fs_expected.add(Em.get()); fs_expected.add(lb.get());
+    ASSERT_EQ(F->getFollowSet().get()->getSet(), fs_expected.getSet());
+    ASSERT_EQ(F_d->getFollowSet().get()->getSet(), fs_expected.getSet());
+    fs_expected.add(ast.get());
+    ASSERT_EQ(P->getFollowSet().get()->getSet(), fs_expected.getSet());
+}
